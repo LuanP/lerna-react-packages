9e4f685feac7291a863a1f7090489557d330d6df
Luan Pablo <luanpab@gmail.com>
2019-11-18 00:50:09 -0500

    Fix styleguide

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './packages/index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts(x)?$/,
+        use: [
+          'ts-loader'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.tsx',
+      '.ts'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+}
+
+module.exports = config
